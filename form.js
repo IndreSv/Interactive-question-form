7316a1cb16299fa44c69204ad933ed88f01128a8
Indre Sveikauskaite <indresveikauskaite@Indres-MacBook-Air.local>
2017-06-10 22:57:25 +0300

    getting started with angularjs

--- form.js
+++ form.js
@@ -1,7 +1,8 @@
 var app = angular.module('myApp', []);
 
 app.controller('mainCtrl', function($scope) {
-    $scope.test = function() {
-        console.log("Hello there! This the helloWorld controller function, in the mainCtrl!");
+    $scope.showMe = false;
+    $scope.myFunc = function() {
+        $scope.showMe = !$scope.showMe;
     }
-};);
+});
