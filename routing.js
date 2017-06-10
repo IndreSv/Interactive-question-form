912d8708689d45b6bf7f1fc2901958ee2525cc44
Indre Sveikauskaite <indresveikauskaite@Indres-MacBook-Air.local>
2017-06-11 00:48:52 +0300

    getting started with angularjs

--- routing.js
+++ routing.js
@@ -1,1 +1,52 @@
 var app = angular.module('myApp');
+
+.config(['$stateProvider', function ($stateProvider) {
+       var intro = {
+               name: 'intro',
+               url: '/',
+               templateUrl: 'index.html'
+           },
+           first = {
+               name: 'first',
+               url: '/1',
+               parent: intro,
+               templateUrl: 'index.html'
+           },
+           second = {
+               name: 'second',
+               url: '/2',
+               parent: intro,
+               templateUrl: 'index.html'
+           },
+           third = {
+               name: 'third',
+               url: '/3',
+               parent: intro,
+               templateUrl: 'index.html'
+           };
+           fourth = {
+               name: 'fourth',
+               url: '/4',
+               parent: intro,
+               templateUrl: 'index.html'
+           };
+           fith = {
+               name: 'fith',
+               url: '/5',
+               parent: intro,
+               templateUrl: 'index.html'
+           };
+           summary = {
+               name: 'summary',
+               url: '/summary',
+               parent: intro,
+               templateUrl: 'index.html'
+           };
+       $stateProvider.state(intro);
+       $stateProvider.state(first);
+       $stateProvider.state(second);
+       $stateProvider.state(third);
+       $stateProvider.state(fourth);
+       $stateProvider.state(fith);
+       $stateProvider.state(summary);
+   }])
