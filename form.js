1d4276f37a1b5b711dce0bc3f611dcf8b0c66fcf
Indre Sveikauskaite <indresveikauskaite@Indres-MacBook-Air.local>
2017-06-12 23:15:56 +0300

    still getting started with angularjs

--- form.js
+++ form.js
@@ -2,27 +2,43 @@ var app = angular.module('myApp', ['ui.router']);
 
 
 app.controller('mainCtrl', function($scope, $state) {
-  $scope.tabs = ['intro', 'first', 'second', 'third', 'fourth', 'fith', 'summary'];
-  $scope.currentIndex = -1;
-$scope.nextq = function () {
-  $scope.currentIndex = $scope.currentIndex+1;
-  var name = $scope.tabs[$scope.currentIndex];
-  $state.go(name);
-}
+    $scope.tabs = ['intro', 'first', 'second', 'third', 'fourth', 'fith', 'summary'];
+    $scope.currentIndex = -1;
+    $scope.nextq = function() {
+        $scope.currentIndex = $scope.currentIndex + 1;
+        var name = $scope.tabs[$scope.currentIndex];
+        $state.go(name);
+    }
 });
 
 
-app.controller('check', function MyCtrl($scope) {
-  $scope.value = "none" ;
-  $scope.isChecked = false;
-  $scope.checkStuff = function () {
-      $scope.isChecked = !$scope.isChecked;
-  }
+app.directive('check', function() {
+    return {
+        controller: function($scope) {
+            $scope.value = "none";
+            $scope.isChecked = false;
+            $scope.checkStuff = function() {
+                $scope.isChecked = !$scope.isChecked;
+            }
+        }
+    };
 });
+// 
+// app.directive('getValue', function() {
+//         return {
+//             controller: function($scope) {
+//                 $scope.change = 'data';
+//                 $scope.getVal = function() {
+//                     console.log($scope.value);
+//                     $scope.change = $scope.value;
+//                 }
+//             }
+//         }
+//     });
 
-// TODO:
-// - Unhide questions one by one (and hide previous one)
-// - Validations (if "next" clicked without choosing option)
-// - One dynamic question (question 4)
-// - Storing the chosen options
-// - Showing everything in summary
+            // TODO:
+            // - Unhide questions one by one (and hide previous one)
+            // - Validations (if "next" clicked without choosing option)
+            // - One dynamic question (question 4)
+            // - Storing the chosen options
+            // - Showing everything in summary
