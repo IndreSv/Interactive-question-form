0e2b42ccce846e0ca5193adebcbe12dfe8c05828
Indre Sveikauskaite <indresveikauskaite@Indres-MacBook-Air.local>
2017-07-20 18:18:06 +0300

    controller for storing data

--- controllers/data.js
+++ controllers/data.js
@@ -2,53 +2,19 @@ angular.module("myApp").controller("newctrl", newController);
 
 newController.$inject = [
   "$scope",
-  "CalculatorService"
+  "storeData"
 ];
 
-function newController($scope, CalculatorService) {
-  $scope.findSquare = function () {
-     $scope.answer = CalculatorService.square($scope.number);
+function newController($scope, storeData) {
+  $scope.saveAnswer = function() {
+      storeData.setData($scope.answer1);
+      console.log($scope.answer1);
+  }
    }
-}
 
 
-
-// app.controller("add", ['sharedList', function(sharedList) {
-//   var vm = this;
-//   vm.text = "";
-//   vm.addAnswer = function() {
-//     // Stuff the item into the shared list
-//     sharedList.addItem(vm.text);
-//     vm.text = "";
-//   }
-// }]);
-
-// app.controller("getInfo", ['sharedList', function(sharedList) {
-//   var vm = this;
-//   // Read the data
-//   vm.listItems = sharedList.getList();
-// }]);
-
-//
-// app.directive('check', function() {
-//     return {
-//         controller: function($scope) {
-//             $scope.value = "none";
-//             $scope.isChecked = false;
-//             $scope.checkStuff = function() {
-//                 $scope.isChecked = !$scope.isChecked;
-//             }
-//         }
-//     };
-// });
-
-//try this: https://docs.angularjs.org/api/ng/directive/ngValue ?
-
-// app.controller('Add', function(){
-//    var vm = this;
-//    vm.answers = [];
-//    vm.Add = function() {
-//      vm.answers.push({value});
-//      console.log(vm.answers);
+// function newController($scope, CalculatorService) {
+//   $scope.findSquare = function () {
+//      $scope.answer = CalculatorService.square($scope.number);
 //    }
-// });
+// }
--- srvc.js
+++ srvc.js
@@ -1,11 +1,26 @@
-// var CalculatorService = angular.module('CalculatorService', [])
-// .service('testService', function () {
-//     this.square = function (a) { return a*a};
-//     console.log("hello");
-//
-// });
+
+// angular.module('myApp')
+//     .service('CalculatorService', function() {
+//         this.square = function(a) {
+//             return a * a
+//         };
+//     });
+
 
 angular.module('myApp')
-    .service('CalculatorService', function() {
-    this.square = function (a) { return a*a};
-  });
+    .factory('storeData', function() {
+        var answer1 = '';
+
+        function setData(a) {
+          console.log(a);
+answer1 = a;
+        }
+        return {
+            getData: function() {
+              return {
+                answer1: answer1
+              }
+            },
+            setData: setData
+        }
+    })
