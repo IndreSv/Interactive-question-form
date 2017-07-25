23740f6a2ca7427875d45fa98883a76cfa8fc175
Indre Sveikauskaite <indresveikauskaite@Indres-MacBook-Air.local>
2017-07-25 17:13:19 +0300

    saving data

--- controllers/data.js
+++ controllers/data.js
@@ -1,15 +1,41 @@
+// angular.module("myApp").controller("newctrl", newController);
+//
+// newController.$inject = [
+//   "$scope",
+//   "storeData"
+// ];
+//
+// function newController($scope, storeData) {
+//   $scope.saveAnswer = function() {
+//      storeData.setData($scope.answer1);
+//   }
+//   $scope.getAnswer = function() {
+//     $scope.answer1 = storeData.getData();
+//   }
+//    }
+
 angular.module("myApp").controller("newctrl", newController);
 
 newController.$inject = [
-  "$scope",
-  "storeData"
+    "$scope",
+    "storeData"
 ];
 
 function newController($scope, storeData) {
-  $scope.saveAnswer = function() {
-     storeData.setData($scope.answer1);
-  }
-  $scope.getAnswer = function() {
-    $scope.answer1 = storeData.getData();
-  }
-   }
+    $scope.saveAnswer1 = function() {
+        storeData.answer1.set($scope.answer1);
+        console.log($scope.answer1);
+    };
+    $scope.saveAnswer2 = function() {
+        storeData.answer2.set($scope.answer2);
+        console.log($scope.answer2);
+    };
+    $scope.getAnswer1 = function() {
+        var answer1 = storeData.answer1.get();
+        $scope.answer1 = answer1;
+    };
+    $scope.getAnswer2 = function() {
+        var answer2 = storeData.answer2.get();
+        $scope.answer2 = answer2;
+    };
+}
--- srvc.js
+++ srvc.js
@@ -1,18 +1,47 @@
-angular.module('myApp')
-    .factory('storeData', function() {
-        var answer1 = '';
-        var answer2 = '';
+angular.module('myApp').factory('storeData', StoreDataService);
 
-        function setData(a) {
-          console.log(a);
-answer1 = a;
-        }
-        return {
-            getData: function() {
-              return {
-                answer1: answer1,
-              }
+function StoreDataService() {
+    var answer1;
+var answer2;
+    return {
+        answer1: {
+            set: function(newValue) {
+                answer1 = newValue;
             },
-            setData: setData
+            get: function() {
+                return answer1;
+            }
+        },
+        answer2: {
+            set: function(newValue) {
+                answer2 = newValue;
+            },
+            get: function() {
+                return answer2;
+            }
         }
-    })
+    }
+}
+
+// angular.module('myApp')
+//     .factory('storeData', function() {
+//       var answers = {
+//         answer1: "",
+//         answer2: ""
+//       }
+//
+//         function setData(a, b) {
+//           console.log(a, b);
+// answer1 = a;
+// answer2 = b;
+//         }
+//         return {
+//             getData: function() {
+//               return {
+//                 answer1: answer1,
+//                 answer2: answer2
+//               }
+//             },
+//             setData: setData
+//         }
+//     })
