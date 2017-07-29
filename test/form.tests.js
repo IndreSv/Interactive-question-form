4889dafe8b011f7c02b752b9049d1cce74f56325
Indre Sveikauskaite <indresveikauskaite@Indres-MacBook-Air.local>
2017-07-29 23:55:51 +0300

    tests files added

---
+++ test/form.tests.js
@@ -0,0 +1,4 @@
+describe("firstQ", function() {
+beforeEach(module('myApp'));
+
+})
---
+++ test/karma-conf.js
@@ -0,0 +1,52 @@
+module.exports = function(config){
+    config.set({
+    //  root path location that will be used to resolve all relative paths in files and exclude sections, should be the root of your project
+    basePath : '../',
+
+    // files to include, ordered by dependencies
+    files : [
+      // include relevant Angular files and libs
+      'bower-angular/angular.js',
+      'Controllers/form.js',
+      // 'test/lib/angular/angular-mocks.js',
+
+      // include js files
+      '*.js',
+      'Controllers/*.js',
+
+      // include unit test specs
+      'test/*.js'
+    ],
+    // files to exclude
+    exclude : [
+      'app/lib/angular/angular-loader.js'
+      , 'app/lib/angular/*.min.js'
+      , 'app/lib/angular/angular-scenario.js'
+    ],
+
+    // karma has its own autoWatch feature but Grunt watch can also do this
+    autoWatch : false,
+
+    // testing framework, be sure to install the karma plugin
+    frameworks: ['jasmine'],
+
+    // browsers to test against, be sure to install the correct karma browser launcher plugin
+    browsers : ['Chrome', 'PhantomJS', 'Firefox'],
+
+    // progress is the default reporter
+    reporters: ['progress'],
+
+    // map of preprocessors that is used mostly for plugins
+    preprocessors: {
+
+    },
+
+    // list of karma plugins
+    plugins : [
+        'karma-junit-reporter',
+        'karma-chrome-launcher',
+        'karma-firefox-launcher',
+        'karma-jasmine',
+        'karma-phantomjs-launcher'
+    ]
+})}
