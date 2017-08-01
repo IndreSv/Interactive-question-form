9d36dc66c0158e478a9f271df9f03786db434bf8
Indre Sveikauskaite <indresveikauskaite@Indres-MacBook-Air.local>
2017-08-01 15:19:54 +0300

    some more comments fixed

--- test/form.tests.js
+++ test/form.tests.js
@@ -17,104 +17,48 @@ describe("statesTesting", function() {
         $scope = _$rootScope_.$new();
     }));
 
+    beforeEach(function() {
+        $controller("stateChangeController", {
+            $scope: $scope,
+            $state: $state
+        })
+    });
+
     describe('$scope.nextq', function() {
         it('should be directed to first state', function() {
-            ctrl = $controller("firstQ", {
-                $scope: $scope,
-                $state: $state
-            });
             spyOn($state, 'go');
-            $scope.nextq();
+            $scope.question1();
             expect($state.go).toHaveBeenCalledWith('first');
         })
-
 
         it('should be directed next to second state', function() {
-            ctrl = $controller("secondQ", {
-                $scope: $scope,
-                $state: $state
-            });
             spyOn($state, 'go');
-            $scope.nextq();
+            $scope.question2();
             expect($state.go).toHaveBeenCalledWith('second');
         })
 
-
-        it('should be directed back to first state', function() {
-            ctrl = $controller("thirdQ", {
-                $scope: $scope,
-                $state: $state
-            });
-            spyOn($state, 'go');
-            $scope.previousq();
-            expect($state.go).toHaveBeenCalledWith('first');
-        })
         it('should be directed to third state', function() {
-            ctrl = $controller("thirdQ", {
-                $scope: $scope,
-                $state: $state
-            });
             spyOn($state, 'go');
-            $scope.nextq();
+            $scope.question3();
             expect($state.go).toHaveBeenCalledWith('third');
-        })
-        it('should be directed back to second state', function() {
-            ctrl = $controller("fourthQ", {
-                $scope: $scope,
-                $state: $state
-            });
-            spyOn($state, 'go');
-            $scope.previousq();
-            expect($state.go).toHaveBeenCalledWith('second');
         })
 
         it('should be directed to fourth state', function() {
-            ctrl = $controller("fourthQ", {
-                $scope: $scope,
-                $state: $state
-            });
             spyOn($state, 'go');
-            $scope.nextq();
+            $scope.question4();
             expect($state.go).toHaveBeenCalledWith('fourth');
         })
-        it('should be directed back to third state', function() {
-            ctrl = $controller("fifthQ", {
-                $scope: $scope,
-                $state: $state
-            });
-            spyOn($state, 'go');
-            $scope.previousq();
-            expect($state.go).toHaveBeenCalledWith('third');
-        })
 
-        it('should be directed to fith state', function() {
-            ctrl = $controller("fifthQ", {
-                $scope: $scope,
-                $state: $state
-            });
+        it('should be directed to fifth state', function() {
             spyOn($state, 'go');
-            $scope.nextq();
-            expect($state.go).toHaveBeenCalledWith('fith');
+            $scope.question5();
+            expect($state.go).toHaveBeenCalledWith('fifth');
         })
-
-    })
-    it('should be directed back to fith state', function() {
-        ctrl = $controller("dynamicQ", {
-            $scope: $scope,
-            $state: $state
-        });
-        spyOn($state, 'go');
-        $scope.previousq();
-        expect($state.go).toHaveBeenCalledWith('fourth');
-    })
-    it('should be directed to final state', function() {
-        ctrl = $controller("summaryView", {
-            $scope: $scope,
-            $state: $state
-        });
-        spyOn($state, 'go');
-        $scope.nextq();
-        expect($state.go).toHaveBeenCalledWith('summary');
+        it('should be directed to final state', function() {
+            spyOn($state, 'go');
+            $scope.summary();
+            expect($state.go).toHaveBeenCalledWith('summary');
+        })
     })
 
 })
