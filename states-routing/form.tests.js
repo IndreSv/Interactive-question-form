428abc9e718715028a6e1459b5befd7bef9ffd80
Indre Sveikauskaite <indresveikauskaite@Indres-MacBook-Air.local>
2017-08-23 22:42:41 +0300

    changed incorrect link

--- states-routing/form.tests.js
+++ states-routing/form.tests.js
@@ -2,61 +2,60 @@ describe("statesTesting", function() {
     var stateProviderMock, ctrl;
     beforeEach(function() {
         stateProviderMock = jasmine.createSpyObj('stateProvider', ['intro']);
-        scopeProviderMock = jasmine.createSpyObj('scope', ['nextq', 'previousq']);
     })
     beforeEach(module('myApp', function($provide) {
         $provide.value('$stateProvider', stateProviderMock);
     }));
     var $controller;
     var $state;
-    var $scope;
+    var vm;
 
-    beforeEach(inject(function(_$controller_, _$state_, _$rootScope_) {
+    beforeEach(inject(function(_$controller_, _$state_) {
         $controller = _$controller_;
         $state = _$state_;
-        $scope = _$rootScope_.$new();
+        vm = this;
     }));
 
     beforeEach(function() {
         $controller("stateChangeController", {
-            $scope: $scope,
-            $state: $state
+            $state: $state,
+            vm: this
         })
     });
 
     describe('states tests', function() {
         it('should be directed to first state', function() {
             spyOn($state, 'go');
-            $scope.question1();
+            vm.question1();
             expect($state.go).toHaveBeenCalledWith('first');
         })
 
         it('should be directed next to second state', function() {
             spyOn($state, 'go');
-            $scope.question2();
+          vm.question2();
             expect($state.go).toHaveBeenCalledWith('second');
         })
 
         it('should be directed to third state', function() {
             spyOn($state, 'go');
-            $scope.question3();
+            vm.question3();
             expect($state.go).toHaveBeenCalledWith('third');
         })
 
         it('should be directed to fourth state', function() {
             spyOn($state, 'go');
-            $scope.question4();
+            vm.question4();
             expect($state.go).toHaveBeenCalledWith('fourth');
         })
 
         it('should be directed to fifth state', function() {
             spyOn($state, 'go');
-            $scope.question5();
+            vm.question5();
             expect($state.go).toHaveBeenCalledWith('fifth');
         })
         it('should be directed to final state', function() {
             spyOn($state, 'go');
-            $scope.summary();
+            vm.summary();
             expect($state.go).toHaveBeenCalledWith('summary');
         })
     })
